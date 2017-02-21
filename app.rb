cc206cff5a3b17e76b2f0b99a10ecaeb1bf25c19
Sai To Yeung <saitoyeung@gmail.com>
2017-02-21 15:33:50 -0500

    Add CORS headers and allow posting of item

--- app.rb
+++ app.rb
@@ -3,15 +3,22 @@ require 'json'
 
 before do
   headers 'Access-Control-Allow-Origin' => '*'
+  headers 'Access-Control-Allow-Methods' => 'POST, GET'
+  headers 'Access-Control-Allow-Headers' => 'Authorization,Accepts,Content-Type,X-CSRF-Token,X-Requested-With'
 end
 
 items = []
 
 get '/items' do
-  return JSON.generate(items)
+  JSON.generate(items)
 end
 
 post '/item' do
+  request.body.rewind
   items << JSON.parse(request.body.read)
-  return 200
+  status 201
+end
+
+options '/item' do
+
 end
